bffade4ba68dc91a00fcd4f9fa2a4e8eeb82052f
Manuel Koch <manuel.koch@frontify.com>
2019-09-25 13:25:54 +0200

    css endpoint which dynamically concats all css rules inside css folder

--- sql/databaseconnaction.php
+++ sql/databaseconnaction.php
@@ -1,11 +1,13 @@
 <?php
-    $servername = "localhost";
-    $username = "aless";
-    $password = "123456789";
-    $dbname = "immobilien";
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "yourpassword";
+    $dbname = "imobilien";
     $conn = new mysqli($servername, $username, $password, $dbname);
     $conn->set_charset('utf8');
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+
+    global $conn;
 ?>
