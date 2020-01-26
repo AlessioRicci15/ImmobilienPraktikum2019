037c6e03af8b5d365b234081f8c26fb9bcb4c144
Alessio Ricci <ale.ricci@hispeed.ch>
2020-01-26 19:34:32 +0100

    order

--- home.php
+++ home.php
@@ -1,10 +1,10 @@
 <article>
     <h2>News</h2>
     <br><hr><br><br>
-    <?php include("articles/Wilkommen.php"); ?>
-    <br><br><br><hr><br><br>
-    <?php include("articles/Frontify.php"); ?>  
-    <br><br><br><hr><br><br>  
     <?php include("articles/Erweiterung.php"); ?>  
     <br><br><br><hr><br><br> 
+    <?php include("articles/Frontify.php"); ?>  
+    <br><br><br><hr><br><br>  
+    <?php include("articles/Wilkommen.php"); ?>
+    <br><br><br><hr><br><br>
 </article>
