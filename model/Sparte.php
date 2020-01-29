a44a9bb51b89d75bf68a6e3d7b5b1ef7bd2a44fd
Alessio Ricci <ale.ricci@hispeed.ch>
2020-01-29 12:14:02 +0100

    Model sparte

---
+++ model/Sparte.php
@@ -0,0 +1,32 @@
+<?php
+    class Sparte
+    {
+        private $Sparten_ID;
+        private $Sparte;
+        function setSparten_ID($newSparten_ID){
+            $this->Sparten_ID = $newSparten_ID;
+        }
+        public function getSparten_ID(){
+            return $this->Sparten_ID;
+        }
+        function setSparte($newSparte){
+            $this->Sparte = $newSparte;
+        }
+        function getSparte(){
+            return $this->Sparte;
+        }
+        public function orderedparten(){
+            global $conn;
+            $spartenliste = "SELECT `sparte`.`Sparten_ID`, 
+                                   `sparte`.`Sparte`
+                            FROM `sparte`
+                            ORDER BY `Sparte` ASC";
+            $result = $conn->query($spartenliste);
+            $sparten = [];
+            while ($row = $result->fetch_assoc()) {
+                array_push($sparten, $row);
+            }
+            return $sparten;
+        }
+    }
+?>
