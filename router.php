ac5fba69b3d2a8c9ef347c2434daad614365a4b1
Alessio Ricci <ale.ricci@hispeed.ch>
2020-01-29 12:09:49 +0100

    rerout

--- router.php
+++ router.php
@@ -36,24 +36,180 @@ if ($view === 'api') {
                 $view = 'properties.php';
                 break;
             case 'stocks':
-                $pageTitle = 'Stocks';
-                $navigation = 'stocks';
-                $view = 'stocks.php';
+                switch ($function) {
+                    case '':
+                        $pageTitle = 'Stocks';
+                        $navigation = 'stocks';
+                        $view = 'stocks.php';
+                        break;
+                    case 'show':
+                        $pageTitle = 'Stock';
+                        $navigation = '';
+                        $view = 'stock/show.php';
+                        break;
+                    case 'edit':
+                        $pageTitle = 'Edit stock';
+                        $navigation = '';
+                        $view = 'stock/edit.php';
+                        break;
+                    case 'sendeditstockformular':
+                        $pageTitle = 'Stock changing';
+                        $navigation = '';
+                        $view = 'stock/sendeditstockformular.php';
+                        break;
+                    case 'remove':
+                        $pageTitle = 'Remove stock';
+                        $navigation = '';
+                        $view = 'stock/remove.php';
+                        break;
+                    case 'create':
+                        $pageTitle = 'Create stock';
+                        $navigation = '';
+                        $view = 'stock/create.php';
+                        break;
+                    case 'sendcreatstockformular':
+                        $pageTitle = 'Stock creating';
+                        $navigation = '';
+                        $view = 'stock/sendcreatstockformular.php';
+                        break;
+                    default:
+                        $pageTitle = 'ERROR 404';
+                        $navigation = '';
+                        $view = '404.php';
+                        break;
+                };
                 break;
             case 'bonds':
-                $pageTitle = 'Bonds';
-                $navigation = 'bonds';
-                $view = 'bonds.php';
+                switch ($function) {
+                    case '':
+                        $pageTitle = 'Bonds';
+                        $navigation = 'bonds';
+                        $view = 'bonds.php';
+                        break;
+                    case 'show':
+                        $pageTitle = 'Bond';
+                        $navigation = '';
+                        $view = 'bond/show.php';
+                        break;
+                    case 'edit':
+                        $pageTitle = 'Edit bond';
+                        $navigation = '';
+                        $view = 'bond/edit.php';
+                        break;
+                    case 'sendeditbondformular':
+                        $pageTitle = 'Bond changing';
+                        $navigation = '';
+                        $view = 'bond/sendeditbondformular.php';
+                        break;
+                    case 'remove':
+                        $pageTitle = 'Remove bond';
+                        $navigation = '';
+                        $view = 'bond/remove.php';
+                        break;
+                    case 'create':
+                        $pageTitle = 'Create bond';
+                        $navigation = '';
+                        $view = 'bond/create.php';
+                        break;
+                    case 'sendcreatbondformular':
+                        $pageTitle = 'Bond creating';
+                        $navigation = '';
+                        $view = 'bond/sendcreatbondformular.php';
+                        break;
+                    default:
+                        $pageTitle = 'ERROR 404';
+                        $navigation = '';
+                        $view = '404.php';
+                        break;
+                };
                 break;
             case 'reits':
-                $pageTitle = 'REITs';
-                $navigation = 'reits';
-                $view = 'reits.php';
+                switch ($function) {
+                    case '':
+                        $pageTitle = 'REITs';
+                        $navigation = 'reits';
+                        $view = 'reits.php';
+                        break;
+                    case 'show':
+                        $pageTitle = 'REIT';
+                        $navigation = '';
+                        $view = 'reit/show.php';
+                        break;
+                    case 'edit':
+                        $pageTitle = 'Edit REIT';
+                        $navigation = '';
+                        $view = 'reit/edit.php';
+                        break;
+                    case 'sendeditreitformular':
+                        $pageTitle = 'REIT changing';
+                        $navigation = '';
+                        $view = 'reit/sendeditreitformular.php';
+                        break;
+                    case 'remove':
+                        $pageTitle = 'Remove REIT';
+                        $navigation = '';
+                        $view = 'reit/remove.php';
+                        break;
+                    case 'create':
+                        $pageTitle = 'Create REIT';
+                        $navigation = '';
+                        $view = 'reit/create.php';
+                        break;
+                    case 'sendcreatreitformular':
+                        $pageTitle = 'REIT creating';
+                        $navigation = '';
+                        $view = 'reit/sendcreatreitformular.php';
+                        break;
+                    default:
+                        $pageTitle = 'ERROR 404';
+                        $navigation = '';
+                        $view = '404.php';
+                        break;
+                };
                 break;
             case 'etfs':
-                $pageTitle = 'ETFs';
-                $navigation = 'etfs';
-                $view = 'etfs.php';
+                switch ($function) {
+                    case '':
+                        $pageTitle = 'ETFs';
+                        $navigation = 'etfs';
+                        $view = 'etfs.php';
+                        break;
+                    case 'show':
+                        $pageTitle = 'ETF';
+                        $navigation = '';
+                        $view = 'etf/show.php';
+                        break;
+                    case 'edit':
+                        $pageTitle = 'Edit ETF';
+                        $navigation = '';
+                        $view = 'etf/edit.php';
+                        break;
+                    case 'sendeditetfformular':
+                        $pageTitle = 'ETF changing';
+                        $navigation = '';
+                        $view = 'etf/sendeditetfformular.php';
+                        break;
+                    case 'remove':
+                        $pageTitle = 'Remove ETF';
+                        $navigation = '';
+                        $view = 'etf/remove.php';
+                        break;
+                    case 'create':
+                        $pageTitle = 'Create ETF';
+                        $navigation = '';
+                        $view = 'etf/create.php';
+                        break;
+                    case 'sendcreatetfformular':
+                        $pageTitle = 'ETF creating';
+                        $navigation = '';
+                        $view = 'etf/sendcreatetfformular.php';
+                        break;
+                    default:
+                        $pageTitle = 'ERROR 404';
+                        $navigation = '';
+                        $view = '404.php';
+                        break;
+                };
                 break;
             case 'properties':
                 switch ($function) {
